ffb8fa2132ba3fe9430999d39c0ae24efd71dc68
Abhisheknm <abhi577shek221@gmail.com>
2026-06-25 16:57:09 +0530

    python basics

---
+++ test.py
@@ -0,0 +1,15 @@
+print("Hello world", "world",sep="#")
+print("hello world", "world",sep="f")
+x=10.5
+x="10.5"
+print(int(float(x)))
+print("\test\ntesting")
+print(r"\test\ntesting")
+print("\\test\\ntesting")
+print("""hello
+      my name is
+      John
+      """)
+x="hello"
+print(x[0:3:2])
+print(x[::-1])
